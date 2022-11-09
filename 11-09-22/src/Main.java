d6559955edd1a38ab0db1d11ca4a82c0a7a6b674
138530 <138530@COMLABV1-16.vis.umtc.um>
2022-11-09 14:10:38 +0800

    Pakyu

--- 11-09-22/src/Main.java
+++ 11-09-22/src/Main.java
@@ -1,20 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Imports
+import java.util.Scanner;
 
-/**
- *
- * @author 138530
- */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        double finalPrice = 0.0;
+        int originalPrice, markedUpPrice, sellingPrice = 0, taxRate;
+        
+        // Getting Inputs
+        System.out.print("Enter original price: ");
+        originalPrice = input.nextInt();
+        System.out.print("Enter price of the marked up price: ");
+        markedUpPrice = input.nextInt();
+        System.out.print("Enter sales tax rates: ");
+        taxRate = input.nextInt();
+        
+        // Calculations
+        
+        
+        // Printing/Output
+        System.out.println("Original Price: " + originalPrice);
+        System.out.println("Marked up Percentage: " + markedUpPrice);
+        System.out.println("Selling Price: " + sellingPrice);
+        System.out.println("Tax Rate: " + taxRate);
+        System.out.println("Final Price: " + finalPrice);
     }
     
 }
